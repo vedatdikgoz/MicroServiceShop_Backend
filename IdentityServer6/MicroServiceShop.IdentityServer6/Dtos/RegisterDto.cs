5837791489a324491b5491f025b83d904873bdf2
Vedat Dikgöz <vedat@example.com>
2026-09-20 15:21:13 +0300

    chore: clean package refs, fix nullability, remove legacy IdentityServer, update caching package versions

--- IdentityServer6/MicroServiceShop.IdentityServer6/Dtos/RegisterDto.cs
+++ IdentityServer6/MicroServiceShop.IdentityServer6/Dtos/RegisterDto.cs
@@ -2,10 +2,10 @@
 {
     public class RegisterDto
     {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Password { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
--- IdentityServer6/MicroServiceShop.IdentityServer6/Models/ApplicationUser.cs
+++ IdentityServer6/MicroServiceShop.IdentityServer6/Models/ApplicationUser.cs
@@ -5,7 +5,7 @@ namespace MicroServiceShop.IdentityServer6.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
     }
 }
